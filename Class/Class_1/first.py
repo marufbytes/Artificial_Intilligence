fb502de43821719d5a39cf66d3770a36e196f636
marufbytes <marufahammed215@gmail.com>
2026-02-23 13:06:16 +0600

    first commit

---
+++ Class/Class_1/first.py
@@ -0,0 +1,14 @@
+print("Hello world!")
+
+a=2
+b=3
+c=a+b
+print("Result:", c)
+
+x=float (input("Enter the first number: "))
+ 
+y=float (input("Enter the second number: "))
+
+z=x+y
+
+print (z)
---
+++ Class/Class_1/user_input.py
@@ -0,0 +1,7 @@
+x=float (input("Enter the first number: "))
+ 
+y=float (input("Enter the second number: "))
+
+z=x+y
+
+print (z)
