cb36778097bd3634b27378589e425fb2a40c4e54
marufbytes <marufahammed215@gmail.com>
2026-03-06 01:44:09 +0600

    Swapping

---
+++ Practice/swap.py
@@ -0,0 +1,10 @@
+
+num1= float(input("Enter the first number: "))
+num2 = float (input("Enter the second number: "))
+
+print("Before swapping: ", num1, num2)
+
+num1, num2 = num2, num1
+
+print("After Swapping: ", num1, num2)
+
