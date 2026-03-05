5a9d0162c951a6d8f2ed71929046dd2572270658
marufbytes <marufahammed215@gmail.com>
2026-03-06 00:54:32 +0600

    array

---
+++ Practice/Sum_Even_Num.py
@@ -0,0 +1,9 @@
+# arr = list(map(int, input("Enter the array: ").split()))
+
+arr = list(map(int, input("Enter the array: ").split()))
+sum=0;
+for i in arr:
+    if i%2==0:
+        sum+=i
+
+print(sum)
