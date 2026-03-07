7d0c87d921c326965be0d1cd5f4a8021456c8802
marufbytes <marufahammed215@gmail.com>
2026-03-07 13:51:02 +0600

    Prime using Else

--- Practice/tempCodeRunnerFile.py
+++ Practice/tempCodeRunnerFile.py
@@ -1,18 +1,3 @@
-# Swap Two Numbers
 
-# # Input two numbers
-# num1 = float(input("Enter the first number: "))
-# num2 = float(input("Enter the second number: "))
-
-# # Print values before swap
-# print("Before swapping:")
-# print("First number:", num1)
-# print("Second number:", num2)
-
-# # Swapping the values
-# num1, num2 = num2, num1
-
-# # Print values after swap
-# print("\nAfter swapping:")
-# print("First number:", num1)
-# print("Second number:", num2)
+# else:
+#    print(num,"is not a prime number")
