179f9513c7411064578550eb9ded0b411e47f069
marufbytes <marufahammed215@gmail.com>
2026-03-20 22:58:34 +0600

    Class

---
+++ Home/Prime.py
@@ -0,0 +1,11 @@
+num1=int(input("Enter the first number: "))
+num2=int(input("Enter the second number: "))
+
+for num in range(num1,num2+1):
+    while num>0:
+        for i in range(2,num):
+            if num%i==0:
+                break
+
+    else:
+        print(num)    
---
+++ Home/sum_of_digits.py
@@ -0,0 +1,10 @@
+num = int (input("Enter the number: "))
+
+sum=0
+while num>0:
+    rem = num % 10 
+    sum+=rem
+    num //= 10
+    
+
+print("Sum:",sum)
