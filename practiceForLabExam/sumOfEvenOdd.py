e1ebadcb075594f7d8adff40bc357f8e975e280c
marufbytes <marufahammed215@gmail.com>
2026-04-09 19:31:18 +0600

    Practice

---
+++ practiceForLabExam/sumOfEvenOdd.py
@@ -0,0 +1,13 @@
+arr = [2,4,5,3,5,4,6]
+
+sumOfEven=0
+sumOfOdd =0
+for digit in arr:
+    if digit%2==0:
+        sumOfEven+=digit
+
+    else:
+        sumOfOdd+=digit
+
+print(sumOfEven)
+print(sumOfOdd)
